18e0541d86ad50ec9f67996fc2b6e915e2df6d49
SEOKKAMONI <sj01048260279@gmail.com>
2023-08-28 23:50:59 +0900

    ADD :: Text Component

---
+++ packages/ui/src/Text/index.tsx
@@ -0,0 +1,46 @@
+import { font } from "@maru/design-token";
+import { CSSProperties, HTMLAttributes, ReactNode } from "react";
+import styled, { css } from "styled-components";
+
+type Font = keyof typeof font;
+
+interface Props extends HTMLAttributes<HTMLSpanElement> {
+  children: ReactNode;
+  color?: string;
+  fontType: Font;
+  width?: CSSProperties["width"];
+  textAlign?: CSSProperties["textAlign"];
+  ellipsis?: boolean;
+}
+
+const Text = ({
+  children,
+  color,
+  fontType,
+  textAlign,
+  width,
+  ellipsis = false,
+}: Props) => {
+  return (
+    <StyledText
+      style={{ color, textAlign, width }}
+      fontType={fontType}
+      ellipsis={ellipsis}
+    >
+      {children}
+    </StyledText>
+  );
+};
+
+export default Text;
+
+const StyledText = styled.p<{ fontType: Font; ellipsis: boolean }>`
+  ${({ fontType }) => font[fontType]}
+  ${(props) =>
+    props.ellipsis &&
+    css`
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    `}
+`;
