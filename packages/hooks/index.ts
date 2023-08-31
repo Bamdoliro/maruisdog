c7b6f0d9d2fedf3b0e9f6ae4aad869351c7d5836
SEOKKAMONI <sj01048260279@gmail.com>
2023-08-31 14:58:17 +0900

    ADD :: useOutsideClick 추가

--- packages/hooks/index.ts
+++ packages/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useBooleanState } from "./src/useBooleanState";
 export { default as useInterval } from "./src/useInterval";
+export { default as useOutsideClick } from "./src/useOutsideClick";
